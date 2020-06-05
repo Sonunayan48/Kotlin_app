5c079d71c94077dfd05c73a7f603c41a9a0ff608
MAYANK KUMAR <sonunayan48@gmail.com>
2020-06-05 15:06:28 +0530

    Winner selection selection configured.

--- app/src/main/java/com/sonunayan48/android/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/sonunayan48/android/kotlinapp/MainActivity.kt
@@ -9,12 +9,13 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var steps: Int = 0
-
+    private var steps: Int = 0
+    private val len = 3
+    private lateinit var arr: Array<IntArray>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        arr = Array(len) { IntArray(len) }
         box00.setOnClickListener { performStep(it, 0, 0) }
         box01.setOnClickListener { performStep(it, 0, 1) }
         box02.setOnClickListener { performStep(it, 0, 2) }
@@ -31,32 +32,84 @@ class MainActivity : AppCompatActivity() {
         val view: TextView = v as TextView
         if ((view.text.toString()).isEmpty()) {
             when (steps % 2) {
-                0 -> view.text = "O"
-                else -> view.text = "X"
+                0 -> {
+                    view.text = "O"
+                    arr[row][col] = 1
+                }
+                else -> {
+                    view.text = "X"
+                    arr[row][col] = 2
+                }
+            }
+            if (checkWin()) {
+                createGameOverDialog(0, view.text.toString())
+                return
             }
             steps++
         }
         if (steps == 9) {
-            createGameOverDialog()
+            createGameOverDialog(1)
         }
     }
 
-    private fun createGameOverDialog() {
+    private fun checkWin(): Boolean {
+        for (i in 0 until len) {
+            if (arr[i][0] == arr[i][1]
+                && arr[i][0] == arr[i][2]
+                && arr[i][0] != 0
+            ) {
+                return true
+            }
+        }
+        for (i in 0 until len) {
+            if (arr[0][i] == arr[1][i]
+                && arr[0][i] == arr[2][i]
+                && arr[0][i] != 0
+            ) {
+                return true
+            }
+        }
+        if (arr[0][0] == arr[1][1]
+            && arr[0][0] == arr[2][2]
+            && arr[0][0] != 0
+        ) {
+            return true
+        }
+        if (arr[0][2] == arr[1][1]
+            && arr[0][2] == arr[2][0]
+            && arr[0][2] != 0
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun createGameOverDialog(i: Int, a: String = "") {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-        builder.setTitle("Game Over!")
-        builder.setMessage("The game has been ended and no one won the game.")
+        builder.setTitle(
+            when (i) {
+                0 -> "Winner!"
+                else -> "Game Over"
+            }
+        )
+        builder.setMessage(
+            when (i) {
+                0 -> "Player $a is the Winner, Congrats!!!"
+                else -> "The game has been ended and no one won the game."
+            }
+        )
         builder.setPositiveButton(
-            "Restart",
-            DialogInterface.OnClickListener { dialogInterface: DialogInterface, i: Int ->
-                dialogInterface.cancel()
-                recreate()
-            })
+            "Restart"
+        ) { dialogInterface: DialogInterface, _: Int ->
+            dialogInterface.cancel()
+            recreate()
+        }
         builder.setNegativeButton(
-            "Exit Game",
-            DialogInterface.OnClickListener { dialog: DialogInterface, which: Int ->
-                dialog.cancel()
-                finishAffinity()
-            })
+            "Exit Game"
+        ) { dialog: DialogInterface, _: Int ->
+            dialog.cancel()
+            finishAffinity()
+        }
         builder.setCancelable(false)
         val dialog: AlertDialog = builder.create()
         dialog.show()
